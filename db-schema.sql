22fbcd515901b4e5a2d9e70dc15e8e22bf9c8f66
Julien Torrent <julien.torrent@outlook.com>
2022-01-12 10:58:29 +0100

    fix: add db-schema to insert hidden tag

---
+++ db-schema.sql
@@ -0,0 +1,4 @@
+-- maybe remove this from here since graasp core is passing it as a plugin option
+-- so maybe graasp core should INSERT this in the DB
+INSERT INTO "tag" ("id", "name", "nested")
+VALUES ('b5373e38-e89b-4dc7-b4b9-fd3601504467', 'hidden', 'allow');
